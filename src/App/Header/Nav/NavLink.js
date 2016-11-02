8444a1469b0ed87af3026cf61993f683e9398410
onaliugo <hello@onali-ugo.fr>
2016-11-02 15:26:26 +0100

    Fix active class on navigation links

--- src/App/Header/Nav/NavLink.js
+++ src/App/Header/Nav/NavLink.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 import './NavLink.css';
 
 const NavLink = (props) =>
-  <Link className="App-navLink" activeClassName="is-active" {...props}>
+  <Link className="App-navLink" activeClassName="is-active" onlyActiveOnIndex={true} {...props}>
     {props.children}
   </Link>
 ;
