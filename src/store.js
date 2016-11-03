5cb55a9ad64f5d125f305ba6151000af23e78470
onaliugo <hello@onali-ugo.fr>
2016-11-03 09:37:52 +0100

    Enable Redux devToolsExtension

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux-immutable';
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
 import reducers from './reducers.js';
 
 const store = createStore(
-  combineReducers(reducers)
+  combineReducers(reducers),
+  compose(
+    // Redux dev tools
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
 );
 
 export default store;
