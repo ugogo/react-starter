22cbc0857e0faa8426a82f7bf23558720a115cdd
onaliugo <hello@onali-ugo.fr>
2017-01-17 21:57:50 +0100

    Use redux-thunk to handle async

--- src/store.js
+++ src/store.js
@@ -1,13 +1,29 @@
-import { compose, createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
+import { Iterable } from "immutable";
 import { combineReducers } from "redux-immutable";
 import reducers from "./reducers.js";
+import thunk from "redux-thunk";
+
+const middlewares = [
+  thunk,
+];
+
+if (process.env.NODE_ENV === "development") {
+  const logger = require("redux-logger")({
+    stateTransformer: (state) =>
+      Iterable.isIterable(state) ? state.toJS() : state
+    ,
+  });
+
+  middlewares.push(logger);
+}
 
 const store = createStore(
   combineReducers(reducers),
   compose(
-    // Redux dev tools
+    applyMiddleware(...middlewares),
     window.devToolsExtension ? window.devToolsExtension() : (f) => f,
-  )
+  ),
 );
 
 export default store;
